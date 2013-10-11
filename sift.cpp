0742286be49536780af2728ddf9b6888df3b1e4c
Austin Walters <lettergramm@gmail.com>
2013-10-10 21:50:23 -0500

    Create sift.cpp

---
+++ sift.cpp
@@ -0,0 +1,23 @@
+#include <opencv/cv.h>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv/highgui.h>
+#include <opencv2/nonfree/nonfree.hpp>
+#include <opencv2/opencv.hpp>
+
+/* version 2.4 of openCV */
+
+int main(int argc, const char* argv[])
+{
+    const cv::Mat input = cv::imread("Small_Galaxy.jpg", 0); //Load as grayscale and pick name
+
+    cv::SIFT detector;
+    std::vector<cv::KeyPoint> keypoints;
+    detector.detect(input, keypoints);
+
+    // Add results to image and save.
+    cv::Mat output;
+    cv::drawKeypoints(input, keypoints, output);
+    cv::imwrite("sift_result.jpg", output);
+
+    return 0;
+}
